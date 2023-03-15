be5e8fbb736e04ea809cb4414860dea0a0779ef4
Xize Wu <wuxize1995@gmail.com>
2023-03-15 18:36:56 -0400

    Input Module

--- play.js
+++ play.js
@@ -1,15 +1,6 @@
 const {print, connect}= require("./client");
+const {setupInput} = require("./input")
 print()
 connect()
+setupInput()
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
- };
