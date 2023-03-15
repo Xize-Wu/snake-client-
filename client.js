48a705cab6f67593ec18d2baabdd33869966db6e
Xize Wu <wuxize1995@gmail.com>
2023-03-15 18:19:31 -0400

    Move Commands

--- client.js
+++ client.js
@@ -6,10 +6,37 @@ const connect = function () {
     host: "165.227.47.243", // IP address here,
     port: 50541// PORT number here,
   });
+  
+  //Upon connection, print the line and write a name
   conn.on("connect",()=>{
     console.log("Connection established!");
     conn.write("Name: XZW");
+  
   })
+
+  //hardcode: move the snake!
+
+  setInterval((arg) =>{
+conn.write(arg)
+  }, 1500,"Move: up","Move: left", "Move: left", "Move: left", "Move: down", "Move: right")
+
+  // setTimeout(() => {
+  //   conn.write( "Move: up" )
+  // }, 2000)
+  
+  // setTimeout(() => {
+  //   conn.write( "Move: left" )
+  // }, 4000)
+  
+  // setTimeout(() => {
+  //   conn.write( "Move: down" )
+  // }, 6000)
+
+
+  // setTimeout(() => {
+  //   conn.write( "Move: right" )
+  // }, 8000)
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
