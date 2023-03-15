b70b0f98edc26b1485b1adeddf4579016eaf6dee
Xize Wu <wuxize1995@gmail.com>
2023-03-15 18:25:21 -0400

    Stdin Setup

--- play.js
+++ play.js
@@ -2,3 +2,14 @@ const {print, connect}= require("./client");
 print()
 connect()
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+ };
