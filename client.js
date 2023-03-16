86f7ab845f0e15b8fbbe966cf6b6317fd126a94d
Xize Wu <wuxize1995@gmail.com>
2023-03-15 20:43:30 -0400

    Constants Module

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function (IPAddress, portNum) {
   const conn = net.createConnection({
-    host: "165.227.47.243", // IP address here,
-    port: 50541// PORT number here,
+    host: IPAddress, // IP address here,
+    port: portNum// PORT number here,
   });
   
   //Upon connection, print the line and write a name
--- play.js
+++ play.js
@@ -1,7 +1,8 @@
 const {print, connect}= require("./client");
+const { IP, PORT } = require("./constants");
 const {setupInput} = require("./input")
 print()
 
 //the object returned by connect() should be passed into setupInput()
-setupInput(connect())
+setupInput(connect(IP, PORT))
 
